f250a765836f4d6350d197e759549723294dcf1f
Renier <renier.trenuela@gmail.com>
2023-07-28 13:15:14 +0800

    update

--- app/Filament/Resources/OrderPaymentResource.php
+++ app/Filament/Resources/OrderPaymentResource.php
@@ -2,16 +2,27 @@
 
 namespace App\Filament\Resources;
 
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Pages\Page;
+use App\Models\OrderPayment;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\BadgeColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\TernaryFilter;
+use Filament\Tables\Actions\DeleteBulkAction;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
 use App\Filament\Resources\OrderPaymentResource\Pages;
 use App\Filament\Resources\OrderPaymentResource\RelationManagers;
-use App\Models\OrderPayment;
-use Filament\Forms;
-use Filament\Resources\Form;
-use Filament\Resources\Resource;
-use Filament\Resources\Table;
-use Filament\Tables;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\OrderPaymentResource\Pages\EditOrderPayment;
+use App\Filament\Resources\OrderPaymentResource\Pages\ListOrderPayments;
+use App\Filament\Resources\OrderPaymentResource\Pages\CreateOrderPayment;
 
 class OrderPaymentResource extends Resource
 {
@@ -23,7 +34,8 @@ class OrderPaymentResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Toggle::make('is_confirmed')
+                    ->disabled(fn ($record) => $record->is_confirmed)
             ]);
     }
 
@@ -31,26 +43,46 @@ class OrderPaymentResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('created_at')->sortable(),
+                TextColumn::make('ref_no')->sortable(),
+                TextColumn::make('payment_method')->sortable(),
+                BadgeColumn::make('is_confirmed')
+                    ->icons([
+                        'heroicon-o-x' => 0,
+                        'heroicon-o-check' => 1,
+                    ])
+
             ])
             ->filters([
-                //
+                TernaryFilter::make('is_confirmed')->default(),
+                Filter::make('created_at')
+                    ->form([
+                        Forms\Components\TextInput::make('ref_no'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['ref_no'],
+                                fn (Builder $query, $refNo): Builder => $query->where('ref_no', 'like', "$refNo%"),
+                            );
+                    })
             ])
+            ->defaultSort('created_at', 'desc')
             ->actions([
-                Tables\Actions\EditAction::make(),
+                //   Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
+                // Tables\Actions\DeleteBulkAction::make(),
             ]);
     }
-    
+
     public static function getRelations(): array
     {
         return [
             //
         ];
     }
-    
+
     public static function getPages(): array
     {
         return [
@@ -58,5 +90,10 @@ class OrderPaymentResource extends Resource
             'create' => Pages\CreateOrderPayment::route('/create'),
             'edit' => Pages\EditOrderPayment::route('/{record}/edit'),
         ];
-    }    
+    }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 }
