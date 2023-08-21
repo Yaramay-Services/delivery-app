c2873c913fb745e28a6181319c5c2df0aa1ed9d0
Renier Dev <renier.trenuela@gmail.com>
2023-08-21 13:07:43 +0800

    update footer

--- Modules/WebApp/Resources/views/layouts/default.blade.php
+++ Modules/WebApp/Resources/views/layouts/default.blade.php
@@ -29,6 +29,7 @@
     <!-- ===============================================-->
     <link href="{{ asset('WebAppTheme/public/assets/css/theme.css') }}" rel="stylesheet" />
     <link href="{{ asset('vendor/fontawesome-free6/css/all.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('WebAppTheme/public/assets/css/theme-footer.css') }}" rel="stylesheet" />
 
     {{-- Laravel Vite - CSS File --}}
     {{-- {{ module_vite('build-webapp', 'Resources/assets/sass/app.scss') }} --}}
@@ -44,37 +45,12 @@
     <main class="main" id="top">
         <livewire:webapp::components.navbar-component>
 
-        {{ $slot }}
-
-        <!-- ============================================-->
-        <!-- <section> begin ============================-->
-        <section class="py-0 pt-7 bg-1000">
-
-            <div class="container">
-                <hr class="border border-800" />
-                <div class="row flex-center pb-3">
-                    <div class="col-md-6 order-0">
-                        <p class="text-200 text-center text-md-start">All rights Reserved &copy; Yaramay, {{ now()->format('Y') }}</p>
-                    </div>
-                    <div class="col-md-6 order-1">
-                        <p class="text-200 text-center text-md-end"> Made with&nbsp;
-                            <svg class="bi bi-suit-heart-fill" xmlns="http://www.w3.org/2000/svg" width="15"
-                                height="15" fill="#FFB30E" viewBox="0 0 16 16">
-                                <path
-                                    d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z">
-                                </path>
-                            </svg>&nbsp;by&nbsp;<a class="text-200 fw-bold" href="https://themewagon.com/"
-                                target="_blank">ThemeWagon </a>
-                        </p>
-                    </div>
-                </div>
-            </div><!-- end of .container-->
-
-        </section>
-        <!-- <section> close ============================-->
-        <!-- ============================================-->
-
-
+            {{ $slot }}
+            <!-- ============================================-->
+            <!-- <section> begin ============================-->
+            @include('webapp::partials.footer')
+            <!-- <section> close ============================-->
+            <!-- ============================================-->
     </main>
     <!-- ===============================================-->
     <!--    End of Main Content-->
